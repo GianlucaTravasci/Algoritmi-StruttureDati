ced368446b0939cbace2a7af7308fe19b561dd89
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-10-27 15:53:20 +0100

    code split

--- StruttureDati/Arrays/array-complexity-test.py
+++ StruttureDati/Arrays/array-complexity-test.py
@@ -1,24 +1,37 @@
 array1 = [1, 2, 3, 4, 5, 6]
-array2 = [23, 7, 8, 9]
+array2 = [2, 7, 8, 9]
 
-def findMatch(arr1, arr2):
-    bag = {}
-    for v in arr1: # O(n)
-        if not v in bag:
-            bag[v] = v
+def copyArrayIntoACache(array):
+    cache = {}
+    for v in array: # O(n)
+        if not v in cache:
+            cache[v] = v
 
-    for v in arr2: # O(n)
-        if v in bag:
+    return cache
+
+def compareArrayAndCache(cache, array):
+    for value in array:
+        if value in cache:
             return True
     return False
 
-def findMatchBruteForce(arr1, arr2):
-    for v1 in arr1:
-        for v2 in arr2:
-            if v1 == v2:
-                return True
-    return False
+def findMatch(arr1, arr2):
+    if arr1 and arr2:
+        cache = copyArrayIntoACache(arr1)
+        result = compareArrayAndCache(cache, arr2)
+        return result
+
+    return 'something is missing'
+
+
+
+#def findMatchBruteForce(arr1, arr2):
+    #for v1 in arr1:
+        #for v2 in arr2:
+            #if v1 == v2:
+                #return True
+    #return False
 
 print(findMatch(array1, array2)) # O(n)
 
-print(findMatchBruteForce(array1, array2)) # O(n^2)
+# print(findMatchBruteForce(array1, array2)) # O(n^2)
