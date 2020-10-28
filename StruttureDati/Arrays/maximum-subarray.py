56451a48f07e0f44483ba7b14aea8fa929c73087
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-10-28 16:08:56 +0100

    fixed a bug

--- StruttureDati/Arrays/maximum-subarray.py
+++ StruttureDati/Arrays/maximum-subarray.py
@@ -2,12 +2,12 @@ def maxSubArray(arr):
     max_so_far = arr[0]
     current_max = arr[0]
 
-    for i in range(1, len(arr)-1):
+    for i in range(1, len(arr)):
         current_max = max(arr[i], current_max + arr[i])
         max_so_far = max(max_so_far, current_max)
 
     return max_so_far
 
-a = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+a = [-2, 1, -3, 4, -1, 2, 1, -5, 4, 7, 23]
 
 print(maxSubArray(a))
