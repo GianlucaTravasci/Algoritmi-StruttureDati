13433ec0bf8fddac09830a8f05e18bd4f33dd89a
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-10-28 10:15:10 +0100

    find duplicate in an array with complexity O(n)

---
+++ StruttureDati/Arrays/find-repetition-in-array.py
@@ -0,0 +1,19 @@
+array1 = [1, 2, 3, 1]
+
+array2 = [1, 2, 3, 4]
+
+array3 = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+
+def findDuplicate(arr):
+    cache = {}
+    for value in arr:
+        if value in cache:
+            return True
+        cache[value] = value
+    return False
+
+print(f'array1: {findDuplicate(array1)}')
+
+print(f'array2: {findDuplicate(array2)}')
+
+print(f'array3: {findDuplicate(array3)}')
