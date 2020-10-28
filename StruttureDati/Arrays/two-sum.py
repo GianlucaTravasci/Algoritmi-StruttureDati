3db55efa8a570b6ffca9c9ea86f1441453f851c8
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-10-28 16:39:48 +0100

    two sum solution

---
+++ StruttureDati/Arrays/two-sum.py
@@ -0,0 +1,12 @@
+input = [2, 7, 11, 15]
+
+sum = 9
+
+def twoSum(arr, target):
+    cache = {}
+    for i in range(len(arr)):
+        if target-arr[i] in cache:
+            return [cache[target-arr[i]], i]
+        else:
+            cache[arr[i]] = i
+print(twoSum(input, sum))
