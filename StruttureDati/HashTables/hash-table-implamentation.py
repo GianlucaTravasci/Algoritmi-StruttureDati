a13093a8983580e1acaf5a9cf34c97341ae46892
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-11-07 16:40:04 +0100

    add some comments to the hash table implementation

--- StruttureDati/HashTables/hash-table-implamentation.py
+++ StruttureDati/HashTables/hash-table-implamentation.py
@@ -1,7 +1,23 @@
+# Hash Tables are data structures which generally provide very fast(O(1)) lookups, insertions and deletions
+# In Python, dictionaries are implemented as hash tables.
+
+# The way hashing works is that there is a bucket containing slots to fill with elements. Like in arrays,
+# elements are referenced by their integer indexes, in dictionaries, or hash tables, values are referenced by their
+# keys, which can be of any data type. Now there are different kinds of hash functions (eg: MD5, SHA1, SHA256) which
+# are used to convert the keys into hashes, which are unique for each key And the hashes are then mapped to some slot
+# in the bucket. And the key and value pair get stored in the slot, or in some accompanying data structure within the
+# slot (like, linked lists)
+
+# In general, the lookup, insert and delete operations all are very fast, in the order of O(1)
+# But in some cases, more than one keys can map to the same slot and that increases the time complexity by some margin,
+# although not by a lot in most cases. This is known as a collision.
+# Now, like for almost all problem there is some sort of a solution in the computer science world,
+# collisions can also be resolved by numerous collison resolution techniques like open addressing and closed addressing
 class HashTable:
     def __init__(self, size):
         self.size = size
-        self.data = [[] for i in range(self.size)] #we init the hashtable like and array of length 'size' filled with value none
+        self.data = [[] for i in
+                     range(self.size)]  # we init the hashtable like and array of length 'size' filled with value none
 
     def __str__(self):
         return str(self.__dict__)
@@ -9,7 +25,8 @@ class HashTable:
     def _hash(self, key):
         hash = 0
         for i in range(len(key)):
-            hash = (hash + ord(key[i])*i) % self.size #ord(key[i]) gives the unicode code point of the current character at key[i]
+            hash = (hash + ord(
+                key[i]) * i) % self.size  # ord(key[i]) gives the unicode code point of the current character at key[i]
         return hash
 
     def set(self, key, value):
@@ -27,11 +44,11 @@ class HashTable:
                     return self.data[hash][i][1]
         return []
 
-    def keys(self): #a function that returns all the keys in the list
+    def keys(self):  # a function that returns all the keys in the list
         keysArray = []
         for i in range(self.size):
             if self.data[i]:
-                if len(self.data[i])>1:
+                if len(self.data[i]) > 1:
                     for j in range(len(self.data[i])):
                         keysArray.append(self.data[i][j][0])
                 else:
@@ -42,7 +59,7 @@ class HashTable:
         valuesArray = []
         for i in range(self.size):
             if self.data[i]:
-                if len(self.data[i])>1:
+                if len(self.data[i]) > 1:
                     for j in range(len(self.data[i])):
                         valuesArray.append(self.data[i][j][1])
                 else:
@@ -67,6 +84,7 @@ class HashTable:
                 if reference[i][0] == key:
                     reference[i][1] = newVal
 
+
 new_hash = HashTable(6)
 new_hash.set('one', 1)
 new_hash.set('one', 2)
@@ -79,4 +97,4 @@ print(new_hash)
 print(new_hash.keys())
 print(new_hash.values())
 
-#print(new_hash)
+# print(new_hash)
