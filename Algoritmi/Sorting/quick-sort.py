6ade4d6d95088ecd7de1ad5342d63313a3c940dd
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-11-10 13:46:12 +0100

    add the quicksort description and reformati some code and fixed some typo

--- Algoritmi/Sorting/quick-sort.py
+++ Algoritmi/Sorting/quick-sort.py
@@ -1,30 +1,37 @@
+# Quick Sort is another sorting algorithm which follows divide and conquer approach. It requires to chose a pivot,
+# then place all elements smaller than the pivot on the left of the pivot and all elements larger on the right Then
+# the array is partitioned in the pivot position and the left and right arrays followthe same procedure until the
+# base case is reached. After each pass the pivot element occupies its correct position in the array. Time Complexity
+# in Best and Average cases is O(nlog N) whereas in worst case it jumps up to O(n^2). Space complexity is O(log n)
+
+
 def quicksort(array, left, right):
     if left < right:
         pivot = right
-        partitionindex = partition(array, pivot, left, right)
+        partition_index = partition(array, pivot, left, right)
 
-        quicksort(array, left, partitionindex - 1)
-        quicksort(array, partitionindex + 1, right)
+        quicksort(array, left, partition_index - 1)
+        quicksort(array, partition_index + 1, right)
     return array
 
 
 def partition(array, pivot, left, right):
-    pivotvalue = array[pivot]
-    partitionindex = left
+    pivot_value = array[pivot]
+    partition_index = left
 
     for i in range(left, right):
-        if array[i] < pivotvalue:
-            swap(array, i, partitionindex)
-            partitionindex += 1
+        if array[i] < pivot_value:
+            swap(array, i, partition_index)
+            partition_index += 1
 
-    swap(array, right, partitionindex)
-    return partitionindex
+    swap(array, right, partition_index)
+    return partition_index
 
 
-def swap(array, firstindex, secondindex):
-    temp = array[firstindex]
-    array[firstindex] = array[secondindex]
-    array[secondindex] = temp
+def swap(array, first_index, second_index):
+    temp = array[first_index]
+    array[first_index] = array[second_index]
+    array[second_index] = temp
 
 
 numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
